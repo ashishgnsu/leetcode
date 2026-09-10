a13b2df227605f401c500610c874de467f3d3b16
Ashish Kumar <ashishmaurya9234@gmail.com>
2026-09-10 22:26:45 +0530

    Time: 0 ms (100%), Space: 12.3 MB (91.84%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -8,11 +8,22 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
 
-        first_zero_index = nums1.index(0) if 0 in nums1 else -1
-        if first_zero_index != -1:
-            j = 0
-            for i in range(m, len(nums1)):
-                nums1[i] = nums2[j]
-                j = j+1   
-            nums1.sort()
-        return nums1
+        index = m + n -1
+        i = m-1
+        j = n-1
+        while (i>=0 and j >=0):
+            if nums1[i] > nums2[j]:
+                nums1[index] = nums1[i] 
+                i -= 1
+            else:
+                nums1[index] = nums2[j]
+                j -= 1  
+            index = index -1   
+
+        while j >= 0:
+            nums1[index] = nums2[j]
+            j = j-1
+            index -= 1
+        return nums1    
+
+
